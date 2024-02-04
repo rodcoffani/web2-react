eab552bac620998f0f4a041f765efc937520fa8b
gabscastilho <gabrielly_castilho@outlook.com>
2024-02-04 10:10:38 -0300

    add disciplinas na grade

--- src/components/Pages/Organizar_Semestres/DisciplinasSemestreEmAnalise.js
+++ src/components/Pages/Organizar_Semestres/DisciplinasSemestreEmAnalise.js
@@ -1,9 +1,14 @@
+import MostrarSemestresGrade from '../Visualizar_disciplinas/MostrarDisciplinasGrade'
 import styles from './DisciplinasSemestreEmAnalise.module.css'
+import grupos from '../../../disciplinas.json'
+
 
 function DisciplinasSemestreEmAnalise(){
     return(
         <div className={styles.disciplinas_semestre}>
-            
+            {grupos.map((grupo) => {
+                return(<MostrarSemestresGrade grupo={grupo} key={grupo.grupo}/>)
+            })}
         </div>        
     )
 }
---
+++ src/components/Pages/Visualizar_disciplinas/MostrarDisciplinasGrade.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+import CardDisciplinasGrade from '../../Layout/CardDisciplinasGrade'
+
+import styles from './MostrarDisciplinasGrade.module.css'
+
+function MostrarSemestres({grupo}){
+    
+    var disciplinas = grupo.disciplinas, requisitosArray
+
+    const [semestreFechado, setSemestreFechado] = useState(false)
+
+    return(
+        <div className={styles.semestre}>
+            <button className={styles.titulo_semestre} onClick={() => setSemestreFechado(!semestreFechado)}>{grupo.grupo}</button>
+            {!semestreFechado && (
+                <div className={styles.grupo_disciplinas}>
+                    {
+                        disciplinas.map((disciplina) => {
+                            return(<CardDisciplinasGrade nomeDisciplina={disciplina.nome}/>)
+                        })                
+                    }
+                </div>
+            )}
+            {/* <button className={styles.titulo_semestre}>{grupo.grupo}</button> */}
+        </div>
+    )
+}
+
+export default MostrarSemestres
