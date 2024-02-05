95baf39b25b78a111afc746cb0d449ebc3967d2c
Rodrigo Coffani <rodrigo.coffani@gmail.com>
2024-02-05 01:49:47 -0300

    feat: change how list is showed up

--- src/components/Layout/PreRequisito/PreRequisito.js
+++ src/components/Layout/PreRequisito/PreRequisito.js
@@ -6,16 +6,18 @@ import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
 function PreRequisito({requisitos}){
 
     let contentRequisito = (<div>
-        <span className={styles.informacao}>Tem pré-requisito: <FontAwesomeIcon icon={faXmark} className={styles.xmark}/></span>
+        <span className={styles.requisitoDesktop}>Nenhum pré-requisito</span>
+        <span className={styles.requisitoMobile}>Tem pré-requisito: <FontAwesomeIcon icon={faXmark} className={styles.xmark}/></span>
         </div>)
 
-    // {(requisitos.length > 0) ? <FontAwesomeIcon icon={faCheck} className={styles.check}/> : <FontAwesomeIcon icon={faXmark} className={styles.xmark}/>}
-
     if(requisitos.length !== 0){
-        const requisitosArray = requisitos.map((requisito) =>
-            <div className={styles.informacao}>{requisito}</div>
+        const requisitosArray = requisitos.map((requisito, index) =>
+            index === requisitos.length - 1 ?
+            <div key={index} className={styles.informacao}>{requisito}</div> :
+            <div key={index} className={styles.informacao}>{requisito}, </div>
         )
-        contentRequisito = (<div><div className={styles.requisitoDesktop}>{requisitosArray}</div><span className={styles.requisitoMobile}>Tem pré-requisito: <FontAwesomeIcon icon={faCheck} className={styles.check}/></span></div>)
+
+        contentRequisito = (<div><div className={styles.requisitoDesktop}>Pré-requisitos:{requisitosArray}</div><span className={styles.requisitoMobile}>Tem pré-requisito: <FontAwesomeIcon icon={faCheck} className={styles.check}/></span></div>)
     }
     return <p className={styles.informacao}>{contentRequisito}</p>
 }
