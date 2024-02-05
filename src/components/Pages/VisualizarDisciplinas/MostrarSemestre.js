44815580761a782d0554c239c33a4bf567b37169
Rodrigo Coffani <rodrigo.coffani@gmail.com>
2024-02-05 10:27:47 -0300

    fix: hotfix

--- src/components/Pages/VisualizarDisciplinas/MostrarSemestre.js
+++ src/components/Pages/VisualizarDisciplinas/MostrarSemestre.js
@@ -6,16 +6,11 @@ import styles from './MostrarSemestre.module.css'
 
 function MostrarSemestres({grupo}){
     
-<<<<<<< Updated upstream:src/components/Pages/Visualizar_disciplinas/MostrarSemestre.js
     var disciplinas = grupo.disciplinas, requisitosArray
-
-=======
->>>>>>> Stashed changes:src/components/Pages/VisualizarDisciplinas/MostrarDisciplinasGrade.js
     const [semestreFechado, setSemestreFechado] = useState(false)
     
     return(
         <div className={styles.semestre}>
-<<<<<<< Updated upstream:src/components/Pages/Visualizar_disciplinas/MostrarSemestre.js
             <button className={styles.titulo_semestre} onClick={() => setSemestreFechado(!semestreFechado)}>{grupo.grupo}</button>
             {!semestreFechado && (
                 <div className={styles.grupo_disciplinas}>
@@ -28,16 +23,6 @@ function MostrarSemestres({grupo}){
                 </div>
             )}
             {/* <button className={styles.titulo_semestre}>{grupo.grupo}</button> */}
-=======
-            <div className={styles.grupo_disciplinas}>
-                {grupo && 
-                    grupo.disciplinas.map((disciplina) => {
-                        return(<CardDisciplinasGrade nomeDisciplina={disciplina.nome} siglaDisciplinas={disciplina.sigla} key={disciplina.codigo}/>)
-                    })                
-                }
-                {!grupo && (<div>Carregando</div>)}
-            </div>
->>>>>>> Stashed changes:src/components/Pages/VisualizarDisciplinas/MostrarDisciplinasGrade.js
         </div>
     )
 }
