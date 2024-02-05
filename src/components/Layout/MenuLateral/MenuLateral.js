bb46b7a104989ed432f7e6854d549d166b56f7cc
Rodrigo Coffani <rodrigo.coffani@gmail.com>
2024-02-05 10:06:37 -0300

    feat: add logout button

--- src/components/Layout/MenuLateral/MenuLateral.js
+++ src/components/Layout/MenuLateral/MenuLateral.js
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom'
 
 
 function MenuLateral({ handleMenu, menuAberto}){
+  function logoutHandler() {
+    sessionStorage.clear();
+    window.location.reload();
+  }
+
   return (
     <ul className={`${styles.menu_lateral} ${menuAberto ? styles.active : ''}`}>
-      <li ><Link className={styles.link} to={'/'} onClick={handleMenu}>Visualizar Disciplinas</Link></li>
-      <li ><Link className={styles.link} to={'/OrganizarSemestres'} onClick={handleMenu}>Organizar Semestres</Link></li>
+      <li><Link className={styles.link} to={'/'} onClick={handleMenu}>Visualizar Disciplinas</Link></li>
+      <li><Link className={styles.link} to={'/OrganizarSemestres'} onClick={handleMenu}>Organizar Semestres</Link></li>
+      <li><a href='#' className={`${styles.desktop} ${styles.link}`} onClick={logoutHandler}>Logout</a></li>
     </ul>
   )
 }
--- src/components/Layout/NavBar/NavBar.js
+++ src/components/Layout/NavBar/NavBar.js
@@ -5,6 +5,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 function NavBar({handleMenu}){
+  function logoutHandler() {
+    sessionStorage.clear();
+    window.location.reload();
+  }
+
   return(
     <nav className={styles.navbar}>
     <button onClick={handleMenu} className={styles.icon_button} id='botao_menu'><FontAwesomeIcon icon={faBars}/></button>
@@ -14,6 +19,7 @@ function NavBar({handleMenu}){
     <input type='text' placeholder='Buscar Disciplina' className={`${styles.search_bar} ${styles.desktop}`}/>
     <Link className={`${styles.link} ${styles.desktop}`} to={'/'}>Visualizar Disciplinas</Link>
     <Link className={`${styles.link} ${styles.desktop}`} to={'/OrganizarSemestres'}>Organizar Disciplinas</Link>
+    <a href='#' className={`${styles.link} ${styles.desktop}`} onClick={logoutHandler}>Logout</a>
     
 
     <button className={styles.icon_button}><FontAwesomeIcon icon={faMagnifyingGlass}/></button> 
